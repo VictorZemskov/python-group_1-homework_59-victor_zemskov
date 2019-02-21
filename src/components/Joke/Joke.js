2c90873a10c545749d9504d8721795855bf747a3
Victor <vzemskov090583@gmail.com>
2019-02-21 14:02:31 +0600

    Added 'ADD JOKE'

--- src/components/Joke/Joke.js
+++ src/components/Joke/Joke.js
@@ -2,9 +2,12 @@ import React, {Component} from 'react';
 import './Joke.css';
 
 class Joke extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('[Joke] ShouldUpdate');
+    return nextProps.value !== this.props.value;
+  }
 
   render() {
-    // console.log('[Post] Render');
     return (
       <article className="Joke">
         <h1>{this.props.value}</h1>
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -5,12 +5,7 @@ import Button from '../../components/Button/Button';
 
 class Blog extends Component {
   state = {
-    jokes: [
-      // {title: 'Test Post', author: 'John Doe', id: '1'},
-      // {title: 'Hello, world', author: 'Jack Black', id: '2'},
-      // {title: 'Another example', author: 'Main Editor', id: '3'},
-    ],
-    // postsFormShown: false,
+    jokes: []
   };
 
   componentDidMount() {
@@ -21,51 +16,25 @@ class Blog extends Component {
       }
       throw new Error('Something went wrong with network request');
     }).then(jokes => {
-      const updatedJokes = [jokes];
+      const newJokes = jokes;
+      const updatedJokes = [...this.state.jokes.map(joke => {
+        return {
+            ...joke
+        }
+      })];
+      updatedJokes.push(newJokes);
       this.setState({jokes: updatedJokes});
     }).catch(error => {
       console.log(error);
     })
   }
 
-  changeJoke = () => {
+  addJoke = () => {
       this.componentDidMount();
   };
 
-  // togglePostsForm = () => {
-  //   this.setState(prevState => {
-  //     console.log('[Blog] Toggling form');
-  //     return {postsFormShown: !prevState.postsFormShown};
-  //   });
-  // };
-
-
-  // constructor(props) {
-  //   super(props);
-  //   console.log('[Blog] constructor');
-  //   console.log('[Blog] State exists: ', this.state.posts.length > 0);
-  // }
-  //
-  // componentDidMount() {
-  //   console.log('[Blog] DidMount');
-  // }
-
-
 
   render() {
-    // console.log('[Blog] Render');
-    // console.log(this.state.postsFormShown);
-
-    // let postsForm = null;
-    //
-    // if (this.state.postsFormShown) {
-    //   postsForm = (
-    //     <section className="NewPost">
-    //       <p>New post form will be here</p>
-    //     </section>
-    //   );
-    // }
-
 
     return (
       <Fragment>
@@ -74,15 +43,10 @@ class Blog extends Component {
             <Post
               key={joke.id}
               value={joke.value}
-              // author={post.author}
             />
           ))}
         </section>
-         {/*<button className="ToggleButton" onClick={this.togglePostsForm}>*/}
-           {/*New post*/}
-         {/*</button>*/}
-         {/*{postsForm}*/}
-        <Button btnType="Danger" click={this.changeJoke}>ADD JOKE</Button>
+        <Button btnType="Danger" click={this.addJoke}>ADD JOKE</Button>
        </Fragment>
     )
   }
